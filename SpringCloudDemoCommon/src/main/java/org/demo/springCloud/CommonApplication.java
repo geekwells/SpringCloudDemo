8fb84f270f0d99462bcadae97544b9b2615cc793
geekswg <geekswg@qq.com>
2020-05-30 17:19:42 +0800

    zipkin 服务端已经启动，但是，无法在注册中心注册

--- SpringCloudDemoCommon/src/main/java/org/demo/springCloud/CommonApplication.java
+++ SpringCloudDemoCommon/src/main/java/org/demo/springCloud/CommonApplication.java
@@ -3,12 +3,12 @@ package org.demo.springCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import zipkin2.server.internal.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 
 @SpringBootApplication
 
-//@EnableZipkinServer // 启用 zipkin
+@EnableZipkinServer // 启用 zipkin
 public class CommonApplication {
 
     public static void main(String[] args) {
