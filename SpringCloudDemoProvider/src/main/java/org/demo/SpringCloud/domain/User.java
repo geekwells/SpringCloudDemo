83453b841d9b55300e39795b4a2288c8895324bd
geekswg <geekswg@qq.com>
2020-06-04 07:45:34 +0800

    添加 swagger, 和 smart-doc 两个api文档生成工具 支持

--- SpringCloudDemoProvider/src/main/java/org/demo/SpringCloud/domain/User.java
+++ SpringCloudDemoProvider/src/main/java/org/demo/SpringCloud/domain/User.java
@@ -1,12 +1,28 @@
 package org.demo.SpringCloud.domain;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.lang.NonNull;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+/**
+ * 用户信息
+ */
 @Data
 public class User {
-
+    /**
+     * 用户id
+     */
+    @NotNull
     private String userId;
+    /**
+     * 姓名
+     */
     private String name;
     private String sex;
+
     private int age;
 }
